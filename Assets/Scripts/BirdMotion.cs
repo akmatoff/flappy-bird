5c4f1e54fb51c983aa26c698ab169ab442f31109
unknown <azim.akmatov1@gmail.com>
2020-06-30 17:39:17 +0600

    bird flap

--- Assets/Scripts/BirdMotion.cs
+++ Assets/Scripts/BirdMotion.cs
@@ -1,31 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class BirdMotion : MonoBehaviour
 {
-    // Movement speed
-    public float speed = 2f;
 
     // Flap force
     public float force = 300f;
 
+    private Rigidbody2D bird;
+
     // Start is called before the first frame update
     void Start()
     {
-         GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+        bird = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space)) {
-            GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
+            bird.velocity = UnityEngine.Vector2.zero;
+            bird.AddForce(new UnityEngine.Vector2(0, force));
+
         }
     }
 
-    void OnCollision(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
