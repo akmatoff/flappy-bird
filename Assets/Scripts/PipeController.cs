1bf3e5d5406c8bf20de36016b0ecdb376d632b4e
unknown <azim.akmatov1@gmail.com>
2020-06-30 18:11:04 +0600

    pipe movement

---
+++ Assets/Scripts/PipeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeController : MonoBehaviour
+{
+
+    public float movespeed = 10f;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(-movespeed, 0);
+        if (this.transform.position.x < -15f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
