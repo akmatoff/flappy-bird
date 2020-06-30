a59d2d39695842f6d0d23bbac200ee5e65cd90c1
unknown <azim.akmatov1@gmail.com>
2020-06-30 16:03:23 +0600

    initial commit

---
+++ Assets/Scripts/BirdMotion.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BirdMotion : MonoBehaviour
+{
+    // Movement speed
+    public float speed = 2f;
+
+    // Flap force
+    public float force = 300f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
+        }
+    }
+
+    void OnCollision(Collision2D collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
