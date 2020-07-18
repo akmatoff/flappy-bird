9e6eab790246b100b45813bf9b645164a6b3ff48
unknown <azim.akmatov1@gmail.com>
2020-07-18 20:21:11 +0600

    Started implementing creating objects from JSON

--- Assets/Scripts/LeaderboardManager.cs
+++ Assets/Scripts/LeaderboardManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 using dotenv.net.Utilities;
 using dotenv.net;
 
@@ -10,6 +11,8 @@ public class LeaderboardManager : MonoBehaviour
     string url = "http://flappybaichikapi.akmatoff.repl.co/api/records.json";
     string token;
     string jsonStringArray;
+    public GameObject recordElement;
+    public Transform leaderboardListContent;
     void Start()
     {
         DotEnv.Config(true, ".env"); // Set custom path of the file
@@ -27,10 +30,19 @@ public class LeaderboardManager : MonoBehaviour
 
         if (getRecords.responseCode == 200) {
             Debug.Log("Data fetched!");
-            jsonStringArray = "{\"records\":" + getRecords.downloadHandler.text + "}";
-            Records records = JsonUtility.FromJson<Records>(jsonStringArray);
+            jsonStringArray = "{\"records\":" + getRecords.downloadHandler.text + "}"; // Wrap JSON Array
+            Records records = JsonUtility.FromJson<Records>(jsonStringArray); // Parse JSON Array
+
+
+            // Loop through each record
             foreach(var record in records.records) {
                 print(record.player + ", " + record.highscore);
+                Vector2 position = new Vector2(this.transform.position.x, this.transform.position.y);
+                Quaternion q = this.transform.rotation;
+                recordElement.gameObject.GetComponentInChildren<RecordElement>().playerNameText.GetComponent<Text>().text = record.player;
+                GameObject newRecordObject = Instantiate(recordElement, position, q);
+                newRecordObject.transform.SetParent(leaderboardListContent);
+
             }
         } else {
             Debug.Log(getRecords.error);
---
+++ Assets/Scripts/RecordElement.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecordElement : MonoBehaviour
+{
+    public GameObject playerNameText;
+    public GameObject highscoreText;
+
+}
