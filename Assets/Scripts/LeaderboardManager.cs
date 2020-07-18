3b93ac48a0d080a10601f403cc853fe1a9d7d022
unknown <azim.akmatov1@gmail.com>
2020-07-18 12:37:17 +0600

    dotenv implemented

--- Assets/Scripts/LeaderboardManager.cs
+++ Assets/Scripts/LeaderboardManager.cs
@@ -2,28 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
-using Newtonsoft.Json;
+using dotenv.net.Utilities;
+using dotenv.net;
 
 public class LeaderboardManager : MonoBehaviour
 {
     string url = "http://flappybaichikapi.akmatoff.repl.co/api/records/";
-    string token = "8d0b3ca88b4f22b0eb717590bd3dc4312d7a4feb";
+    string token;
     void Start()
     {
-        StartCoroutine(FetchRecords());
+        DotEnv.Config(true, ".env"); // Set custom path of the file
+        var envReader = new EnvReader(); 
+        token = envReader.GetStringValue("TOKEN"); // Get string from dotenv file
+        StartCoroutine(FetchRecords()); 
     }
 
     IEnumerator FetchRecords() {
-        UnityWebRequest getRecords = UnityWebRequest.Get(url);
-        getRecords.SetRequestHeader("Content-Type", "application/json");
+        UnityWebRequest getRecords = UnityWebRequest.Get(url); // Get request to the API
+        getRecords.SetRequestHeader("Content-Type", "application/json"); // Setting the headers
         getRecords.SetRequestHeader("Accept", "application/json");
-        getRecords.SetRequestHeader("Authorization", "Token " + token);
+        getRecords.SetRequestHeader("Authorization", $"Token {token}");
         yield return getRecords.SendWebRequest();
 
         if (getRecords.responseCode == 200) {
             Debug.Log("Data fetched!");
-            RecordModel record = JsonConvert.DeserializeObject<RecordModel>(getRecords.downloadHandler.text);
-            print(record);
         } else {
             Debug.Log(getRecords.error);
         }
